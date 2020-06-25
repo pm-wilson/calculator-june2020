996eb9e079e0d577f0dec8b21d9adbe3ada120fe
Patrick Wilson <pmwilson333@gmail.com>
2020-06-25 16:04:24 -0700

    does this work in powershell?

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,22 @@
 // IMPORT MODULES under test here:
+import { add } from "../utils.js";
+
 // import example from '../example.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = false;
+test("should return 7 when passed 3 and 4", (expect) => {
+  //Arrange
+  // Set up your arguments and expectations
+  const argument1 = 3;
+  const argument2 = 4;
+  const expected = 7;
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+  //Act
+  // Call the function you're testing and set the result to a const
+  const actual = argument1, argument2;
+
+  //Expect
+  // Make assertions about what is expected versus the actual result
+  expect.equal(actual, expected);
 });
